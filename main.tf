d54b150326cd54310c66d9fdba820083d8076693
Gerson Carneiro <gerson.tpc@outlook.com>
2024-03-03 00:25:04 -0300

    chore: fix addtional tags

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 output "tags" {
-  value = var.additional == "null" ? local.default_tags : merge(local.default_tags, var.custom_tags)
+  value = var.additional == "null" ? local.default_tags : merge(local.default_tags, var.additional)
 }
